cc17a6a2a5158639ba86a0792af22a542fa5a119
Prajwal Murthy <Prajwal Murthy@1labsblr-035-PC>
2021-06-16 12:48:30 +0530

    First Commit...

---
+++ src/main/java/com/prajwal/springboot/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.prajwal.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.prajwal.springboot.services.CoronaVirusDataService;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private CoronaVirusDataService coronaVirusDataService;
+
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("stats", coronaVirusDataService.getAllstats());
+		return "index";
+	}
+	
+}
